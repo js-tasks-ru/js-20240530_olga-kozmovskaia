1e3435989b962c46d6123e171af48bc4ed040b36
OKozmovskaia <olga.kozmovskaia@gmail.com>
2024-06-05 20:26:28 +0200

    Second task: sorted strings (localeCompare)

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,22 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+  let copyArr = [...arr];
 
+  if (param == "asc") {
+    return copyArr.sort((a, b) =>
+      a.localeCompare(b, ["ru", "en"], {
+        sensitivity: "case",
+        caseFirst: "upper",
+      })
+    );
+  } else {
+    return copyArr.sort((a, b) =>
+      b.localeCompare(a, ["ru", "en"], {
+        sensitivity: "case",
+        caseFirst: "upper",
+      })
+    );
+  }
 }
