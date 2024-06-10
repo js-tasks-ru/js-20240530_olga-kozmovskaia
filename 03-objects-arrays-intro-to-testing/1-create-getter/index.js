2ab6d1127165cf8120782a88069525418fcf54f3
OKozmovskaia <olga.kozmovskaia@gmail.com>
2024-06-10 17:37:11 +0200

    Third task: create getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,30 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
+const object = {
+  category: {
+    title: "Goods",
+    foo: undefined,
+  },
+};
 
+export function createGetter(path) {
+  const fields = path.split(".");
+
+  return function getValue(object) {
+    let currentObject = object;
+    let hasOwnProperty = false;
+
+    fields.forEach((key, index) => {
+      if (currentObject.hasOwnProperty(key)) {
+        currentObject = currentObject[key];
+
+        if (index === fields.length - 1) {
+          hasOwnProperty = true;
+        }
+      }
+    });
+
+    return hasOwnProperty ? currentObject : undefined;
+  };
 }
